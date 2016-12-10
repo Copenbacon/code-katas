2194904ab64132a6d364ef63100375e5c82597a5
copenbacon <sclary50@gmail.com>
2016-12-09 22:06:26 -0800

    fixed tests in the cuckoo clock

--- snow-day/test_cuckoo_clock.py
+++ snow-day/test_cuckoo_clock.py
@@ -13,24 +13,26 @@ def test_basics():
     assert fizz_buzz_cuckoo_clock("00:00") == "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
     assert fizz_buzz_cuckoo_clock("12:00") == "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
 
-"""Not sure why this stuff below doesn't work, kept getting fixture 'time' not found error in pytest. So I'm commenting it out."""
 
-# def test_my_fizz_buzz_cuckoo_clock(time):
-#     """Random times tests."""
-#     import random
-#     hours, minutes = map(int, time.split(":"))
-#     sounds = {0: " ".join(["Cuckoo"] * (hours % 12 or 12)), 15: "Fizz Buzz", 30: "Cuckoo", 45: "Fizz Buzz"}
-#     if minutes in sounds:
-#         return sounds[minutes]
-#     return "Fizz" if minutes % 3 == 0 else "Buzz" if minutes % 5 == 0 else "tick"
-#     # first, I want to be sure that 0, 15, 30, and 45 do get included regardless
-#     # of whatever other completely random numbers come up in subsequent tests
-#     for quarter in [0, 15, 30, 45]:
-#         quarter_test = "{:02}:{:02}".format(random.randint(0, 23), quarter)
-#         expected, actual = test_my_fizz_buzz_cuckoo_clock(quarter_test), fizz_buzz_cuckoo_clock(quarter_test)
-#         assert actual == expected
-#     # and some totally-random tests
-#     for i in range(40):
-#         random_test = "{:02}:{:02}".format(random.randint(0, 23), random.randint(0, 59))
-#         expected, actual = test_my_fizz_buzz_cuckoo_clock(random_test), fizz_buzz_cuckoo_clock(random_test)
-#         assert actual == expected
+def my_fizz_buzz_cuckoo_clock(time):
+    """This is some other solution from codewars to test against."""
+    hours, minutes = map(int, time.split(":"))
+    sounds = {0: " ".join(["Cuckoo"] * (hours % 12 or 12)), 15: "Fizz Buzz", 30: "Cuckoo", 45: "Fizz Buzz"}
+    if minutes in sounds:
+        return sounds[minutes]
+    return "Fizz" if minutes % 3 == 0 else "Buzz" if minutes % 5 == 0 else "tick"
+
+
+def test_randos():
+    """Test for randoms."""
+    import random
+    """first, I want to be sure that 0, 15, 30, and 45 do get included regardless of whatever other completely random numbers come up in subsequent tests."""
+    for quarter in [0, 15, 30, 45]:
+        quarter_test = "{:02}:{:02}".format(random.randint(0, 23), quarter)
+        expected, actual = my_fizz_buzz_cuckoo_clock(quarter_test), fizz_buzz_cuckoo_clock(quarter_test)
+        assert actual == expected
+    # and some totally-random tests
+    for i in range(40):
+        random_test = "{:02}:{:02}".format(random.randint(0, 23), random.randint(0, 59))
+        expected, actual = my_fizz_buzz_cuckoo_clock(random_test), fizz_buzz_cuckoo_clock(random_test)
+        assert actual == expected
