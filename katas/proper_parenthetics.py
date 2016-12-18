65209c44d6e76cc18882a71e88386c4fa998c4fb
copenbacon <sclary50@gmail.com>
2016-12-18 13:26:19 -0800

    Using one of this week's data structures.

--- katas/proper_parenthetics.py
+++ katas/proper_parenthetics.py
@@ -5,18 +5,19 @@ Returns 1 for an open set of parentheses,
 0 for a balanced set, and -1 for a
 broken set.
 """
+from dll import DLL
 
 
 def parenthetics(string):
     """The parenthetics module checks for open, broken, or balanced sets."""
-    matches = []
+    matches = DLL()
     for char in string:
         if char == "(":
             matches.append(char)
         if char == ")":
-            if matches == [] or matches.pop() != "(":
+            if matches.head is None or matches.pop() != "(":
                 return -1
-    if matches == []:
+    if matches.head is None:
         return 0
     else:
         return 1
