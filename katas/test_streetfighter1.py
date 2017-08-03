46775f492ee497f1059a2d984fd94994b7173de7
copenbacon <sclary50@gmail.com>
2017-08-03 08:27:24 -0400

    more tests

--- katas/test_streetfighter1.py
+++ katas/test_streetfighter1.py
@@ -30,30 +30,41 @@ def test_right():
     solution = ['E.Honda', 'Blanka', 'Guile', 'Balrog', 'Vega', 'Ryu', 'E.Honda', 'Blanka']
     assert street_fighter_selection(fighters, (0, 0), moves) == solution
 
-test.it("should go up 4 times, always the same")
-moves =  ["up"]*4
-solution = ['Ryu', 'Ryu', 'Ryu', 'Ryu']
-test.assert_equals(street_fighter_selection(fighters,(0,0), moves), solution)
 
-test.it("should go down 4 times, always the same")
-moves =  ["down"]*4
-solution = ['Ken', 'Ken', 'Ken', 'Ken']
-test.assert_equals(street_fighter_selection(fighters,(0,0), moves), solution)
+def test_up():
+    """Should go up 4 times, always the same."""
+    moves = ["up"] * 4
+    solution = ['Ryu', 'Ryu', 'Ryu', 'Ryu']
+    assert street_fighter_selection(fighters, (0, 0), moves) == solution
 
-test.it("should use all 4 directions counter-clockwise twice")
-moves =  ["down","right","up","left"]*2
-solution = ['Ken', 'Chun Li', 'E.Honda', 'Ryu', 'Ken', 'Chun Li', 'E.Honda', 'Ryu']
-test.assert_equals(street_fighter_selection(fighters,(0,0), moves), solution)
 
-test.it("should use all 4 directions clockwise twice")
-moves =  ["up","left","down","right"]*2
-solution = ['Ryu', 'Vega', 'M.Bison', 'Ken', 'Ryu', 'Vega', 'M.Bison', 'Ken']
-test.assert_equals(street_fighter_selection(fighters,(0,0), moves), solution)
+def test_down():
+    """Should go down 4 times, always the same."""
+    moves = ["down"] * 4
+    solution = ['Ken', 'Ken', 'Ken', 'Ken']
+    assert street_fighter_selection(fighters, (0, 0), moves) == solution
 
-test.it("should cover all characters")
-moves =  ["up"]+["left"]*6+["down"]+["right"]*6
-solution = ['Ryu', 'Vega', 'Balrog', 'Guile', 'Blanka', 'E.Honda', 'Ryu', 'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat', 'M.Bison', 'Ken']
-test.assert_equals(street_fighter_selection(fighters,(0,0), moves), solution)
+
+def test_clockwise():
+    """Should use all 4 directions counter-clockwise twice."""
+    moves = ["down", "right", "up", "left"] * 2
+    solution = ['Ken', 'Chun Li', 'E.Honda', 'Ryu', 'Ken', 'Chun Li', 'E.Honda', 'Ryu']
+    assert street_fighter_selection(fighters, (0, 0), moves) == solution
+
+
+def test_clockwise_twice():
+    """Should use all 4 directions clockwise twice."""
+    moves = ["up", "left", "down", "right"] * 2
+    solution = ['Ryu', 'Vega', 'M.Bison', 'Ken', 'Ryu', 'Vega', 'M.Bison', 'Ken']
+    assert street_fighter_selection(fighters, (0, 0), moves) == solution
+
+
+def test_all_chars():
+    """Should cover all characters."""
+    moves = ["up"] + ["left"] * 6 + ["down"] + ["right"] * 6
+    solution = ['Ryu', 'Vega', 'Balrog', 'Guile', 'Blanka', 'E.Honda', 'Ryu', 
+                'Ken', 'Chun Li', 'Zangief', 'Dhalsim', 'Sagat', 'M.Bison', 'Ken']
+    assert street_fighter_selection(fighters, (0, 0), moves) == solution
 
 import random
 test.describe("Random Tests now!")
@@ -71,10 +82,10 @@ def my_street_fighter_selection(fighters, position, moves):
         res.append(fighters[position[0]][position[1]])
     return res
     
-test.it("should work with random values")
+"""Should work with random values."""
 for i in range(40):
     moves = []
     for i in range(random.randint(3,50)):
 	    moves.append(random.choice(opts))
-    test.assert_equals(street_fighter_selection(fighters,(0,0), moves), my_street_fighter_selection(fighters,(0,0), moves))
+    assert street_fighter_selection(fighters,(0,0), moves), my_street_fighter_selection(fighters,(0,0), moves)
 
